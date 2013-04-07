be7313d84b7039786b969b4e26abad28653dd665
Roger Campos <roger@itnig.net>
2013-04-07 03:20:37 +0200

    Cleanup whitespaces

--- lib/phrase.rb
+++ lib/phrase.rb
@@ -5,49 +5,49 @@ require 'colorize'
 
 module Phrase
   autoload :Config, 'phrase/config'
-  
+
   CLIENT_VERSION = "0.1"
   API_VERSION = "v1"
-  
+
   class << self
-    
+
     def config
       Thread.current[:phrase_config] ||= Phrase::Config.new
     end
-    
+
     def config=(value)
       Thread.current[:phrase_config] = value
     end
-    
+
     %w(enabled backend prefix suffix auth_token client_version js_host js_use_ssl cache_key_segments_initial cache_lifetime ignored_keys).each do |method|
       module_eval <<-DELEGATORS, __FILE__, __LINE__ + 1
         def #{method}
           config.#{method}
         end
       DELEGATORS
-    
+
       module_eval <<-DELEGATORS, __FILE__, __LINE__ + 1
         def #{method}=(value)
           config.#{method} = (value)
         end
       DELEGATORS
     end
-    
+
     def enabled?
       enabled
     end
-    
+
     def disabled?
       !enabled
     end
   end
-  
+
   def self.configure
     yield(self.config)
   end
-  
+
   autoload :ViewHelpers, 'phrase/view_helpers'
-  
+
   require 'phrase/api'
   require 'phrase/version'
   require 'phrase/engine'
