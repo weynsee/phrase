fd5580a40f86f4a1c0b8058c6aa161f297b01e29
docstun <manuel.boy@dynport.de>
2013-05-31 11:12:54 +0200

    added support for safe basic html rendering in key names

--- lib/phrase/delegate.rb
+++ lib/phrase/delegate.rb
@@ -17,7 +17,11 @@ module Phrase::Delegate
     
   protected
     def decorated_key_name
-      "#{Phrase.prefix}phrase_#{@display_key}#{Phrase.suffix}"
+      "#{Phrase.prefix}phrase_#{normalized_display_key}#{Phrase.suffix}"
+    end
+
+    def normalized_display_key
+      @display_key.gsub("<", "[[[[[[html_open]]]]]]").gsub(">", "[[[[[[html_close]]]]]]")
     end
   end
 end
