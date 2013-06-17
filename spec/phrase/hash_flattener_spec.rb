7b07ae6074a40c52d6a86607ab02beb637a2257b
docstun <manuel.boy@dynport.de>
2013-06-17 13:08:08 +0200

    added spec for hash flattener

--- spec/phrase/hash_flattener_spec.rb
+++ spec/phrase/hash_flattener_spec.rb
@@ -3,6 +3,17 @@ require 'phrase/hash_flattener'
 
 describe Phrase::HashFlattener do
   describe "#flatten(hash, escape, previous_key=nil, &block)" do
-    it "flattens a given hash"
+    subject do
+      keys = []
+      Phrase::HashFlattener.flatten(hash, escape) do |key, value|
+        keys << key
+      end
+      keys
+    end
+
+    let(:hash) { {"foo" => "bar"} }
+    let(:escape) { "." }
+
+    it { should eql [:foo] }
   end
 end
