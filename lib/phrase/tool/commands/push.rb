7f65e4c40f1bbe01ab3e62f7fbd33617273e964d
Jaap Groeneveld <jgroeneveld@me.com>
2013-07-17 15:14:39 +0200

    added tests for choose_files_to_upload and directories itself arent pushed anymore

--- lib/phrase/tool/commands/push.rb
+++ lib/phrase/tool/commands/push.rb
@@ -3,11 +3,11 @@
 class Phrase::Tool::Commands::Push < Phrase::Tool::Commands::Base
   FORMATS_CONTAINING_LOCALE = %q(po yml qph ts xlf)
   RAILS_DEFAULT_FOLDER = "./config/locales/"
-  
+
   def initialize(options, args)
     super(options, args)
     require_auth_token!
-    
+
     @file_names = @args[1..-1]
     @locale = @options.get(:locale)
     @format = @options.get(:format)
@@ -15,13 +15,13 @@ class Phrase::Tool::Commands::Push < Phrase::Tool::Commands::Base
     @recursive = @options.get(:recursive)
     @update_translations = @options.get(:update_translations)
   end
-  
+
   def execute!
     unless @tags.empty? or valid_tags_are_given?(@tags)
       print_error "Invalid tags: Only letters, numbers, underscores and dashes are allowed"
       exit_command
     end
-    
+
     files = choose_files_to_upload(@file_names, @recursive)
 
     if files.empty?
@@ -31,7 +31,7 @@ class Phrase::Tool::Commands::Push < Phrase::Tool::Commands::Base
       upload_files(files)
     end
   end
-  
+
 private
   def choose_files_to_upload(file_names, recursive=false)
     files = []
@@ -40,14 +40,14 @@ private
       if rails_default_locale_folder_available?
         file_names = [RAILS_DEFAULT_FOLDER]
         print_message "No file or directory specified, using #{RAILS_DEFAULT_FOLDER}"
-      else 
+      else
         print_error "Need either a file or directory:"
         print_error "phrase push FILE"
         print_error "phrase push DIRECTORY"
         exit_command
       end
     end
-    
+
     file_names.each do |file_name|
       if File.directory?(file_name)
         pattern = recursive ? "#{File.expand_path(file_name)}/**/*" : "#{File.expand_path(file_name)}/**"
@@ -55,13 +55,13 @@ private
       else
         files << file_name
       end
-    end 
+    end
 
-    files
+    files.reject { |file| File.directory?(file) }
   end
 
   def upload_files(files)
-    files.each do |file| 
+    files.each do |file|
       if file_exists?(file)
         upload_file(file)
       else
@@ -69,7 +69,7 @@ private
       end
     end
   end
-  
+
   def upload_file(file)
     if file_valid?(file)
       begin
@@ -91,25 +91,25 @@ private
       print_error "Notice: Could not upload #{file} (type not supported)"
     end
   end
-  
+
   def file_content(file)
     content = File.open(file).read
     content = utf16_to_utf8(content) if file_seems_to_be_utf16?(file)
     content
   end
-  
+
   def force_use_of_default_locale?(file_path)
     not Phrase::Formats.file_format_exposes_locale?(file_path)
   end
-  
+
   def utf16_to_utf8(string)
     string.encode("UTF-8", "UTF-16")
   end
-  
+
   def file_seems_to_be_utf16?(file)
     Phrase::Tool::EncodingDetector.file_seems_to_be_utf16?(file)
   end
-  
+
   def file_valid?(filepath)
     extension = filepath.split('.').last
     allowed_file_extensions.include?(extension)
@@ -118,15 +118,15 @@ private
   def file_exists?(file)
     File.exist?(file)
   end
-  
+
   def valid_tags_are_given?(tags)
     tags.all? { |tag| Phrase::Tool::TagValidator.valid?(tag) }
   end
-  
+
   def rails_default_locale_folder_available?
     File.exist?(RAILS_DEFAULT_FOLDER) && File.directory?(RAILS_DEFAULT_FOLDER)
   end
-  
+
   def detect_locale_name_from_file_path(file_path)
     Phrase::Formats.detect_locale_name_from_file_path(file_path)
   end
--- spec/phrase/tool/commands/push_spec.rb
+++ spec/phrase/tool/commands/push_spec.rb
@@ -5,21 +5,21 @@ describe Phrase::Tool::Commands::Push do
   let(:options) { Phrase::Tool::Options.new(args, "push") }
   let(:config) { stub(secret: "secr3t") }
   let(:command) { Phrase::Tool::Commands::Push.new(options, args) }
-  
+
   subject { command }
-  
+
   before(:each) do
     Phrase::Tool::Commands::Push.any_instance.stub(:config).and_return(config)
     Phrase::Tool::Commands::Push.any_instance.stub(:print_message)
     Phrase::Tool::Commands::Push.any_instance.stub(:print_error)
   end
-  
+
   describe "#upload_files(files)" do
     before(:each) do
       subject.stub(:upload_file)
       subject.stub(:file_exists?).and_return(true)
     end
-    
+
     it "uploads each file" do
       subject.should_receive(:upload_file).with("foo.txt")
       subject.should_receive(:upload_file).with("bar.txt")
@@ -27,24 +27,24 @@ describe Phrase::Tool::Commands::Push do
       subject.send(:upload_files, ["foo.txt", "bar.txt"])
     end
   end
-  
+
   describe "#upload_file(file)" do
     let(:api_client) { stub(upload: true) }
     let(:file) { "foo.txt" }
-    
+
     before(:each) do
       subject.stub(:api_client).and_return(api_client)
     end
-    
+
     context "file is invalid" do
       let(:file) { "spec/fixtures/edge/wrongext.doc" }
-      
+
       it "should skip upload" do
         api_client.should_not_receive(:upload)
         subject.send(:upload_file, file)
       end
     end
-    
+
     context "file is valid" do
       context "file provides a locale information" do
         let(:file) { "spec/fixtures/yml/nice.en.yml" }
@@ -54,11 +54,11 @@ describe Phrase::Tool::Commands::Push do
           subject.send(:upload_file, file)
         end
       end
-      
+
       context "file does not provide a locale information" do
         let(:file) { "spec/fixtures/edge/nice.pot" }
         let(:default_locale) { stub(name: "default-locale") }
-        
+
         before(:each) do
           Phrase::Tool::Locale.stub(:find_default_locale).and_return(default_locale)
         end
@@ -70,19 +70,19 @@ describe Phrase::Tool::Commands::Push do
       end
     end
   end
-  
+
   describe "#force_use_of_default_locale?(file_path)" do
     subject { command.send(:force_use_of_default_locale?, file_path) }
-    
+
     context "is a gettext file" do
       let(:file_path) { "./fixtures/formats/translations.en.po" }
-      
+
       it { should be_false }
     end
-    
+
     context "is a gettext pot file" do
       let(:file_path) { "./fixtures/formats/translations.pot" }
-      
+
       it { should be_true }
     end
   end
@@ -116,7 +116,7 @@ describe Phrase::Tool::Commands::Push do
       it { should be_true }
     end
   end
-  
+
   describe "#valid_tags_are_given?(tags)" do
     subject { command.send(:valid_tags_are_given?, tags) }
 
@@ -132,14 +132,44 @@ describe Phrase::Tool::Commands::Push do
       it { should be_false }
     end
   end
-    
+
   describe "#detect_locale_name_from_file_path(file_path)" do
     subject { command.send(:detect_locale_name_from_file_path, file_path) }
-    
+
     context "extension is unknown" do
       let(:file_path) { "test.doc" }
 
       it { should be_nil }
+    end
+  end
+
+  describe "#choose_files_to_upload(file_names, recursive)" do
+    subject { command.send(:choose_files_to_upload, files, recursive) }
+    let(:recursive) { false }
+
+    context "one file" do
+      let(:files) { ["spec/fixtures/yml/nice.en.yml"] }
+
+      it { should == files }
+    end
+
+    context "two files" do
+      let(:files) { ["spec/fixtures/yml/nice.en.yml", "spec/fixtures/yml/nice.de.yml"] }
+
+      it { should == files }
+    end
+
+    context "folder" do
+      let(:files) { ["spec/fixtures/mixed"] }
+
+      it { should == [File.expand_path('spec/fixtures/mixed/nice.yml'), File.expand_path('spec/fixtures/mixed/wrong.yml.rb')] }
+    end
+
+    context "folder recursive" do
+      let(:files) { ["spec/fixtures/mixed"] }
+      let(:recursive) { true }
+
+      it { should == [File.expand_path('spec/fixtures/mixed/nice.yml'), File.expand_path('spec/fixtures/mixed/subfolder/subitem.yml'), File.expand_path('spec/fixtures/mixed/wrong.yml.rb')] }
     end
   end
 end
