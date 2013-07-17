bdbf8b2d2b1568bf8072515ae02e67acfd2a8904
Jaap Groeneveld <jgroeneveld@me.com>
2013-07-17 14:43:53 +0200

    added recursive to show_help

--- lib/phrase/tool/commands/show_help.rb
+++ lib/phrase/tool/commands/show_help.rb
@@ -4,13 +4,13 @@ class Phrase::Tool::Commands::ShowHelp < Phrase::Tool::Commands::Base
   def initialize(options, args)
     super(options, args)
   end
-  
+
   def execute!
     show_help
   end
-  
+
 private
-  
+
   def show_help
     print_message <<USAGE
 usage: phrase <command> [<args>]
@@ -18,7 +18,7 @@ usage: phrase <command> [<args>]
   phrase init --secret=<YOUR SECRET> --default-locale=<DEFAULT LOCALE, e.g. en> --default-format=<FORMAT, e.g. yml> --default-target=<TARGET, default is ./phrase/locales/>
 
   phrase push FILE [--tags=<tags>] [--locale=<locale>]
-  phrase push DIRECTORY [--tags=<tags>] [--locale=<locale>]
+  phrase push DIRECTORY [--tags=<tags>] [--locale=<locale>] [--recursive]
 
   phrase pull [LOCALE] [--target=<target-folder>] [--format=<format>] [--tag=<tag>]
 
