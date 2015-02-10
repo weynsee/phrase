e0ffa29536980c6bb750d174e8aad5a5d6f11359
docstun <manuel.boy@dynport.de>
2015-02-10 17:12:27 +0100

    attempt to fix #51

--- lib/phrase/engine.rb
+++ lib/phrase/engine.rb
@@ -6,8 +6,11 @@ require 'i18n'
 if defined? Rails
   class Phrase::Engine < Rails::Engine
     initializer 'phrase', :after => :disable_dependency_loading do |app|
-      require 'phrase/adapters/i18n'
-      require 'phrase/adapters/fast_gettext'
+      if Phrase.enabled?
+        require 'phrase/adapters/i18n'
+        require 'phrase/adapters/fast_gettext'
+      end
+
       ActionView::Base.send :include, Phrase::ViewHelpers
     end
   end
