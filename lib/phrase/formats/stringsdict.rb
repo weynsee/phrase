3c943d15ef6dc3fcb45f31d0fd578ee0e56004b7
Rich Barton <richard.barton@xing.com>
2015-05-04 09:57:57 +0200

    Adds support for Stringsdict format

---
+++ lib/phrase/formats/stringsdict.rb
@@ -0,0 +1,45 @@
+# -*- encoding : utf-8 -*-
+
+module Phrase
+  module Formats
+    class Stringsdict < Phrase::Formats::Base
+      def self.directory_for_locale(locale)
+        name = locale.code || locale.name
+        "#{formatted(name)}.lproj"
+      end
+
+      def self.filename_for_locale(locale)
+        "Localizable.stringsdict"
+      end
+
+      def self.target_directory
+        "./"
+      end
+
+      def self.extract_locale_name_from_file_path(file_path)
+        file_path.scan(/\/([a-zA-Z\-_]*).lproj\//i).first.try(:first)
+      end
+
+      def self.locale_aware?
+        true
+      end
+
+      # These strings should conform to the same lproj filenames as XCode autogenerates
+      # See complete list here http://www.ibabbleon.com/iOS-Language-Codes-ISO-639.html
+      def self.formatted(name)
+        return name unless name.include?("-")
+        parts = name.split("-")
+        if parts.first.downcase.include?("zh")
+          "#{parts.first.downcase}-#{parts.last.capitalize}"
+        else
+          "#{parts.first}-#{parts.last.upcase}"
+        end
+      end
+      private_class_method :formatted
+
+      def self.extensions
+        [:stringsdict]
+      end
+    end
+  end
+end
